85abb960b6e199426703bcc49e1f35e15e5e8704
cndrw <cowalther2@gmail.com>
2026-08-27 18:14:38 +0200

    Tested timer pwm mode
    refined build script aswell

--- build.bat
+++ build.bat
@@ -1,6 +1,21 @@
 @echo off
 
 cd firmware\build
+
 avr-gcc -Os -mmcu=attiny84a ../src/main.c
+if errorlevel 1 goto :error_compile
+
 avr-objcopy -O ihex -j .text -j .data a.out a.hex   
+
 avrdude -v -V -p t84a -c stk500v1 -PCOM6 -b19200 -U flash:w:a.hex:i
+if errorlevel 1 goto :error_flash
+
+exit /b 0
+
+:error_compile
+echo Compile Error
+exit /b 1
+
+:error_flash
+echo Flashing Failed
+exit /b 1
--- firmware/src/main.c
+++ firmware/src/main.c
@@ -1,21 +1,38 @@
 #define F_CPU 1000000UL
+#define __AVR_ATtiny84A__
+
 #include <avr/io.h>
+#include <avr/interrupt.h>
+
 #include <util/delay.h>
+
+typedef uint8_t u8;
+
+u8 cnt = 0; 
 
 int main()
 {
-    DDRA |= (1 << DDA7);
+    DDRB |= (1 << PB2);
+    TCCR0A |= (1 << WGM00) | (1 << WGM01) | // set fast-pwm mode
+              (1 << COM0A1);                // non-inverting mode (clear oc0a on cmp match)
+
+    TCCR0B |= (1 << CS00) | (1 << CS01);    // clk prescaler to 64 -> 10^6 / 64 = 15625 
+
+    OCR0A = 0xFF;                            // 50% duty cycle
+
+    TIMSK0 |= (1 << OCIE0A);                // enable cmp match interrupt
+
+    sei();  // enable interrupt
 
     while (1)
     {
-        PORTA |= (1 << PORTA7);
-
-        _delay_ms(500);
-
-        PORTA &= ~(1 << PORTA7);
-
-        _delay_ms(500);
-
+        _delay_ms(10);
     }
+
     return 0;
+}
+
+ISR(TIM0_COMPA_vect)
+{
+    OCR0A = cnt++;
 }
